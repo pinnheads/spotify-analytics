0b2302ab502e41849f437c266349b72e0c40c60e
Utsav <utsavdeep01@gmail.com>
2020-06-11 03:28:51 +0530

    Fix Callback URL

--- app.js
+++ app.js
@@ -36,7 +36,8 @@ passport.use(
     {
       clientID: appKey,
       clientSecret: appSecret,
-      callbackURL: 'http://localhost:3000/auth/spotify/callback',
+      callbackURL:
+        'https://arcane-eyrie-81143.herokuapp.com/auth/spotify/callback',
     },
     function (accessToken, refreshToken, expires_in, profile, done) {
       // Set Globals
